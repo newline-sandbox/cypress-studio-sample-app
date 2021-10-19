fac9c46141b7f1b82912ffe5f489b5e74f98f33b
Paige Niedringhaus <paigen11@gmail.com>
2021-10-19 19:02:46 -0400

    adding to cypress test

--- cypress/integration/movie_search_spec.js
+++ cypress/integration/movie_search_spec.js
@@ -1,12 +1,17 @@
 describe('Movie app', () => {
   it('should show all results for movies that contain "Star Wars" in their titles', () => {
     /* ==== Generated with Cypress Studio ==== */
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.get('.navbar-top-links > :nth-child(3) > a').click();
+    cy.get('.search-label').should('contain', 'Search Movie Titles Here:'); // user added
     cy.get('.search-input').clear();
     cy.get('.search-input').type('Star Wars');
     cy.get('[type="submit"]').click();
+    cy.get('.card-component').should('have.length', 20); // user added
     cy.get(':nth-child(2) > .breakpoint__medium-up > .movie-component > .movie-poster').click();
+    cy.get('.movie-details-info__overview').should('contain', 'Movie Overview'); // user added
+    cy.get('div').should('contain', "1977-05-25") // user added
+    cy.get('div').should('contain', "8.2") // user added
     cy.get('.movie-details-reviews > :nth-child(3)').click();
     cy.get('.fa').click();
     /* ==== End Cypress Studio ==== */
